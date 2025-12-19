7baa829bbb21dedd85b14444a0ac10b78a4d502f
varshini28042007@gmail.com <varshini28042007@gmail.com>
2025-12-19 16:16:01 +0530

    Register file uploaded

--- src/main/java/com/ecomerce/repository/UserRepository.java
+++ src/main/java/com/ecomerce/repository/UserRepository.java
@@ -8,11 +8,12 @@ import org.springframework.data.repository.query.Param;
 
 import com.ecomerce.entity.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByUsername(String username);
-    Optional<User> findByEmail(String email);
-    boolean existsByUsername(String username);
-    boolean existsByEmail(String email);
-    @Query("SELECT u FROM User u WHERE u.username = :username OR u.email = :email")
-    Optional<User> findByUsernameOrEmail(@Param("username")String username, @Param("email")String email);
+public class UserRepository extends JpaRepository<User, Long> {
+    public Optional<User> findByUsername(String username);
+    public Optional<User> findByEmail(String email);
+    public boolean existsByUsername(String username);
+    public boolean existsByEmail(String email);
+    @Query("SELECT u FROM User u WHERE u.username = :?1 OR u.email = :?2")
+    public Optional<User> findByUsernameOrEmail(String username,String email);
+
 }
